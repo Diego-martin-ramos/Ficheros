36cf11586230e0cc300c6d04f64a98f1562bc33c
diego.marram.1 <diego.marram.1@ED37009131P499.educa.jcyl.es>
2026-09-25 19:15:02 +0200

    ejercicios Ficheros

---
+++ Ejercicio2Ficheros/src/main/java/com/mycompany/ejercicio2ficheros/Ejercicio2Ficheros.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio2ficheros;
+
+import java.util.List;
+
+/**
+ *
+ * @author diego.marram.1
+ */
+public class Ejercicio2Ficheros {
+
+    public static void main(String[] args) {
+        List<Producto> catalogo=List.off(new Producto("",50.2)
+        new Producto("",50.2));
+        
+    }
+}
